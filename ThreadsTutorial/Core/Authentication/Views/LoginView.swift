9adae7125296910b7dbcd02cbe33e8d4f316920c
Steve Hong <stevehonggg@gmail.com>
2024-02-16 12:03:54 +0900

    Project Setup

---
+++ ThreadsTutorial/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsTutorial
+//
+//  Created by 홍성범 on 2/16/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
