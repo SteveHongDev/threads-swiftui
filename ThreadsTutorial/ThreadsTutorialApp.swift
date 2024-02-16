f677e9f5acd28f09dea0a2d271671d7a270e2bba
Steve Hong <stevehonggg@gmail.com>
2024-02-16 11:57:02 +0900

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by 홍성범 on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
